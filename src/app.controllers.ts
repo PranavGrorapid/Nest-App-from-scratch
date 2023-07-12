2c6504a8dec62a373ed4529123aec888805b6da3
Pranav P <pranavat9948@gmail.com>
2023-07-12 22:46:11 +0530

    bulid a basic nest app from scratch

---
+++ src/app.controllers.ts
@@ -0,0 +1,17 @@
+import { Controller,Get } from "@nestjs/common";
+
+
+@Controller()
+
+ export class AppController{
+
+    @Get()
+
+
+    getRootRouter(){
+
+        return 'hi there'
+    }
+
+
+}
